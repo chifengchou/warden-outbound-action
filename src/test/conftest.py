f050dc9f2f52714be27bcd9f7bd1af8881a59c74
chifengchou <chifeng@horangi.com>
2022-03-08 14:42:24 +0800

    fix conftest

--- src/test/conftest.py
+++ src/test/conftest.py
@@ -1,5 +1,4 @@
 import logging
-from contextvars import ContextVar
 from typing import List
 
 import alembic
@@ -95,8 +94,9 @@ def db_engine():
 
 def get_transaction_level(transaction: SessionTransaction):
     """
-    Get the transaction's level counting from the root. The root is at level 1.
-    NOTE: transaction may not be active. It may not be in a session.
+    Get the transaction's level counting from the root. The root(top level) is
+    at level 1.
+    NOTE: `transaction` may not be active. It also may not be in a session.
     """
     level = 1
     if not transaction.nested:
@@ -110,55 +110,43 @@ def get_transaction_level(transaction: SessionTransaction):
     return level
 
 
-# This stores the level of nested transaction when running a test method.
-# It is initialized when the first test method of a class kicks in. Subsequent
-# test methods of the same class should be in the same level.
-# It is then reset to 0 when all test methods finish.
-function_transaction_level: ContextVar[int] = ContextVar(
-    'function_transaction_level', default=0
-)
-
-
 def restart_savepoint(session, transaction: SessionTransaction):
     """
     A callback responds sqlAlchemy event "after_transaction_end".
-    If the ended transaction is a nested one(2ed or a
-    function_transaction_level). It starts another one at the same level.
+    It starts another one if the ended transaction is a 2ed or 3rd-level.
     """
     level = get_transaction_level(transaction)
-    ft_level = function_transaction_level.get()
     if level == 2:
         session.begin_nested()
         logger.info(f"Class scope begin_nested restarts at {level=}")
-    elif ft_level == level > 2:
+    elif level == 3:
         session.begin_nested()
-        logger.info(f"Function scope begin_nested restarts at {level=}")
+        logger.info(f"Method scope begin_nested restarts at {level=}")
 
 
 @pytest.fixture(scope="class", autouse=True)
 def class_transaction(db_engine):
-    # Setup transaction hierarchy according to this:
-    # https://docs.sqlalchemy.org/en/13/orm/session_transaction.html#joining-a-session-into-an-external-transaction-such-as-for-test-suites  # noqa
-    # Moreover, instead of a 2-level hierarchy of transactions shown in the
-    # link, we will create a 3-level one. This is so that transactional change
-    # in class-level fixture is visible to test methods.
+    # Setup transaction hierarchy.
+    # Ref: https://docs.sqlalchemy.org/en/13/orm/session_transaction.html#joining-a-session-into-an-external-transaction-such-as-for-test-suites  # noqa
     #
-    # NOTE: class-level fixtures assume
-    #  a. it always starts in a top-level transaction
-    #  b. it immediately runs in another top-level transaction after a session
+    # Along with `method_transaction`, we actually create a 3-level hierarchy
+    # instead of a 2-level one shown in the reference. This is so that
+    # transactional change in class-level fixtures is visible to test methods.
+    #
+    # NOTE: a class-level fixture(incl. setup_class/teardown_class) assumes:
+    #  a. it is always in a top-level transaction
+    #  b. it is immediately in another top-level transaction after one is
     #     commit/rollback.
-    #  c. it can begin_nested.
-    #  Therefore, a fixture potentially could:
-    #  1. commit/rollback N times
-    #  2. nest pairs of begin_nested and commit/rollback for M levels
-    #  To allow them, the test framework needs to do the following:
-    #  i. For case 1, once a commit/rollback encloses the current 2ed-level
-    #     transaction, `restart_savepoint` immediately create another 2ed-level
-    #     transaction.
-    #  ii. For case 2, `restart_savepoint` is no-op.
+    #  c. it can begin_nested but must have commit/rollback counterpart.
+    #  We create a 2ed-level transaction for a test class to allow fixtures to
+    #  use it as if it is a top-level. Whenever a 2ed-level is commit/rollback,
+    #  `restart_savepoint` immediately create another one. At the end, we
+    #  rollback the top-level transaction.
+    #  By doing so, fixtures won't be able to commit/rollback on the top-level
+    #  transaction so nothing is persisted to the DB.
     #
     # We don't support a transaction spanning across setup and teardown.
-    # I.e. This is supported:
+    # I.e. This is ok:
     # ```
     # @pytest.fixture(scope="class")
     # def foo():
@@ -175,13 +163,16 @@ def class_transaction(db_engine):
     #     session.commit()  # teardown, intend to enclosing begin_nested
     # ```
 
-    assert function_transaction_level.get() == 0
     # Create a new top-level transaction and 2ed-level nested ones for each
     # class. Everything rolls back when tests in the class finish.
+    logger.info(">> class_transaction setup")
     with db_engine.connect() as connection:
         session_factory.configure(bind=connection)
         transaction = connection.begin()
         session: Session = db_session()
+        # Must not be in autocommit mode(default is False), so session is
+        # implicitly in a top-level transaction.
+        assert not session.autocommit, "Session must not in autocommit mode"
         try:
             session.begin_nested()
             level = get_transaction_level(session.transaction)
@@ -193,64 +184,58 @@ def class_transaction(db_engine):
             event.listen(session, "after_transaction_end", restart_savepoint)
             yield session
         finally:
-            logger.info("All fixtures and test methods in the class finished")
-            assert get_transaction_level(session.transaction) == level + 1, (
-                "Test class may have begin_nested without commit/rollback or "
-                "there is a transaction spanning across setup/teardown"
-            )
-
-            # A begin_nested may be triggered by restart_savepoint again.
-            # But it will be immediately discarded by transaction.rollback.
+            logger.info("<< class_transaction teardown")
             session.close()
+            # A 2ed-level will be start again by `restart_savepoint` after
+            # `session.close()`. But it is immediately discarded because
+            # a new top-level starts.
             logger.info("Connection-level transaction rollback")
             transaction.rollback()
-            function_transaction_level.set(0)
 
 
 @pytest.fixture(scope="function", autouse=True)
-def function_transaction():
-    # Make sure each test method in the class in the same
-    # function_transaction_level.
-    # Rollback function_transaction_level when a test method finishes.
+def method_transaction():
+    # Make sure each test method in the class in 3rd-level transaction.
+    # Rollback when a test method finishes.
     #
-    # NOTE: the test methods assume
-    #  a. it always starts in a top-level transaction
-    #  b. it immediately runs in another top-level transaction after a
-    #     session commit/rollback.
-    #  c. it can begin_nested.
-    #  Therefore, a test method potentially could:
-    #  1. commit/rollback N times
-    #  2. nest pairs of begin_nested and commit/rollback for M levels
-    #  To allow them, the test framework needs to do the following:
-    #  i. Far case 1, once a commit/rollback encloses the current transaction,
-    #     `restart_savepoint` immediately create another
-    #     function_transaction_level transaction.
-    #  ii. For case 2, `restart_savepoint` is no-op.
+    # NOTE: a test method assumes:
+    #  a. it is always in a top-level transaction
+    #  b. it is immediately in another top-level transaction after one is
+    #     commit/rollback.
+    #  c. it can begin_nested but must have commit/rollback counterpart.
+    #  We create a 3rd-level transaction to allow a test method to use it as if
+    #  it is a top-level. Whenever a 3rd-level is commit/rollback,
+    #  `restart_savepoint` immediately create another one. At the end, we
+    #  rollback the 2ed-level transaction.
+    #  By doing so, a test method
+    #  1. won't be able to commit/rollback on the 2ed-level transaction so
+    #     any transactional changes won't be visible to other test methods.
+    #  2. won't be able to commit/rollback on the top-level transaction so
+    #     nothing is persisted to the DB.
+    logger.info(">> method_transaction setup")
     session: Session = db_session()
     assert event.contains(session, "after_transaction_end", restart_savepoint)
 
     # Ensures only the first test method begin_nested from here. Others
     # begin_nested from `restart_savepoint`
-    ft_level = function_transaction_level.get()
-    if ft_level == 0:
-        session.begin_nested()
-        level = get_transaction_level(session.transaction)
-        logger.info(f"Function scope begin_nested starts at {level=}")
-        function_transaction_level.set(level)
-        ft_level = level
-    else:
-        level = get_transaction_level(session.transaction)
-        assert (
-            ft_level == level
-        ), f"Function scope transaction {level}, expected to be {ft_level}"
-        logger.debug(f"Function scope begin_nested at {level=}")
+
+    session.begin_nested()
+    level = get_transaction_level(session.transaction)
+    logger.info(f"Method scope begin_nested starts at {level=}")
+    assert level == 3, f"Method scope transaction {level} != 3"
+
+    parent_transaction = session.transaction.parent
 
     try:
         yield session
     finally:
+        logger.info("<< method_transaction teardown")
         # Ensure that any begin_nested in the test method has
         # its commit/rollback counterpart.
         assert (
-            get_transaction_level(session.transaction) == ft_level
-        ), "Test function may have begin_nested without commit/rollback"
-        session.rollback()
+            get_transaction_level(session.transaction) == 3
+        ), "Test method may have begin_nested without commit/rollback"
+        # A 3rd-level will be start again by `restart_savepoint` after
+        # `parent_transaction.rollback()`. But it is immediately discarded
+        # because a new 2ed-level starts.
+        parent_transaction.rollback()
